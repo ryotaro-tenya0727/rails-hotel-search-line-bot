b57fc9ce2404e1930e8037a683450362fe8e7fe5
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-07-22 01:19:37 +0900

    ADD: Linebotで画像配信

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -3,7 +3,39 @@ class LineBotController < ApplicationController
 
   def callback
     body = request.body.read
-    p body
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          # p event
+          message = {
+            type: 'text',
+            text: "ありがとう"
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          message_id = event['message']['id']
+          response = client.get_message_content(message_id)
+          case response
+          when Net::HTTPSuccess then
+            message = {
+              type: 'image',
+              originalContentUrl: 'https://placebear.com/350/250',
+              previewImageUrl: 'https://placebear.com/350/250',
+            }
+            client.reply_message(event['replyToken'], message)
+          end
+        end
+      end
+    end
+    head :ok
   end
 
   private
@@ -13,5 +45,13 @@ class LineBotController < ApplicationController
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
+  end
+
+  def reply_text(event, texts)
+    texts = [texts] if texts.is_a?(String)
+    client.reply_message(
+      event['replyToken'],
+      texts.map { |text| {type: 'text', text: text} }
+    )
   end
 end
