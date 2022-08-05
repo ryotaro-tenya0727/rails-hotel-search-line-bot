ec83e6645e949d900992d887cce62e885dea50f3
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-08-06 05:41:57 +0900

    Linebotのリファクタリング

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,38 +1,17 @@
 class LineBotController < ApplicationController
   protect_from_forgery except: [:callback]
+  before_action :client
 
   def callback
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
-
-    events = client.parse_events_from(body)
-    events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          # message = search_and_create_message(event.message['text'])
-          message = Apis::RakutenApiClient.client.search_and_create_message(event.message['text'])
-          client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = File.open("/tmp/#{SecureRandom.uuid}.jpg", 'w+b')
-          tf.write(response.body)
-        end
-      end
-    end
+    Apis::LineBotClient.create_message(body, @client, signature)
     head :ok
   end
 
   private
 
   def client
-    @client ||= Line::Bot::Client.new do |config|
-      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-    end
+    @client ||= Apis::LineBotClient.client
   end
 end
---
+++ app/services/apis/line_bot_client.rb
@@ -0,0 +1,31 @@
+class Apis::LineBotClient
+  class << self
+    def create_message(body, client, signature)
+      unless client.validate_signature(body, signature)
+        error 400 do 'Bad Request' end
+      end
+      events = client.parse_events_from(body)
+      events.each do |event|
+        case event
+        when Line::Bot::Event::Message
+          case event.type
+          when Line::Bot::Event::MessageType::Text
+            message = Apis::RakutenApiClient.client.search_and_create_message(event.message['text'])
+            client.reply_message(event['replyToken'], message)
+          when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+            response = client.get_message_content(event.message['id'])
+            tf = File.open("/tmp/#{SecureRandom.uuid}.jpg", 'w+b')
+            tf.write(response.body)
+          end
+        end
+      end
+    end
+
+    def client
+      Line::Bot::Client.new do |config|
+        config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+        config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+      end
+    end
+  end
+end
